89bb595a704781e19e01aa37d9ceb7d0a02e9afc
myar9 <ranamuhammadyaar9@gmail.com>
2022-11-12 16:39:53 +0500

    graph not convert to pdf

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -11,11 +11,15 @@ class PDFController extends Controller
 {
     public function index()
     {
+        ini_set('max_execution_time', 180);
+        // return view('emails.htmlchart');
         $data["email"] = "ranamuhammadyaar9@gmail.com";
         $data["title"] = "From Muhammad Yar";
         $data["body"] = "This is Demo";
 
-        $pdf = PDF::loadView('emails.myTestMail',$data);
+        $pdf = PDF::loadView('emails.htmlchart',$data);
+            //  return $pdf->download('chart.pdf');
+        return $pdf->stream('test.pdf');
 
         Mail::send('emails.myTestMail', $data, function($message)use($data, $pdf) {
             $message->to($data["email"], $data["email"])
---
+++ resources/views/emails/htmlchart.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>CodePen - This Pen is 19% HTML &amp; 81% CSS</title>
+    {{-- <link rel="stylesheet" href="{{ asset('css/style.css') }}"> --}}
+    <style>
+        .newupdate {
+            margin: 20vmin 0 0 50vmin;
+
+
+        }
+
+        .cssnewupdate {
+            margin: 33vmin;
+
+
+        }
+
+        .newpeichat {
+            margin: 5vmin auto;
+            border-radius: 50%;
+            width: 90vmin;
+            height: 90vmin;
+            overflow: hidden;
+            background: linear-gradient(-21deg, #2196f3 50%, transparent 50%), linear-gradient(to right, #2196f3 50%, #ffc107 50%);
+
+        }
+
+        .bodychart {
+            margin: 0;
+            width: 100vw;
+            height: 100vh;
+            font: 5vmin "Arial", sans-serif;
+        }
+    </style>
+
+</head>
+
+<body class="bodychart">
+    <!-- partial:index.partial.html -->
+    <div class="pie newpeichat">
+        <div class="html newupdate">19% HTML</div>
+        <div class="css cssnewupdate">81% CSS</div>
+    </div>
+    <!-- partial -->
+
+</body>
+
+</html>
